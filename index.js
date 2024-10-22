1867c8603744ab76833da81b584204a8199cadb3
Peter Fänge <pellefange@gmail.com>
2024-10-22 17:21:36 +0200

    added .indexOf statement to check if password includes a space

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ let password = prompt("Enter password");
 
 function isValidPassword(username, password) {
     if (password.match(username)) {
-        console.log(false);
+        console.log(false)
+    } else if (password.indexOf(" ") >= 0) {
+        console.log(false)
     } else {
         console.log(true)
     }
